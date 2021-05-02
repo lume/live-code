184371cbc866b866157d8dc81685b38425d70dc3
Joe Pea <joe@trusktr.io>
2021-05-02 13:18:52 -0700

    much more readable example code

--- src/Test.vue
+++ src/Test.vue
@@ -33,6 +33,7 @@
 </template>
 
 <script>
+	import stripIndent from 'strip-indent'
 	import LiveCode from './LiveCode'
 
 	export default {
@@ -40,112 +41,119 @@
 
 		data: () => ({
 			/* eslint-disable no-useless-escape */
-			htmlIframe: /*html*/ `<style>
-    body {
-        background: skyblue;
-        width: 100%;
-        height: 100%;
-    }
-</style>
+			htmlIframe: stripIndent(`
+				<style>
+					body {
+						background: skyblue;
+						width: 100%;
+						height: 100%;
+					}
+				</style>
 
-<script>
-    let count = 0
+				<script>
+					let count = 0
 
-    setInterval(() => {
-        document.body.insertAdjacentHTML(
-            'beforeend',
-            '<br />inside iframe: ' + count++
-        )
-    }, 1000)
-<\/script>
-`,
+					setInterval(() => {
+						document.body.insertAdjacentHTML(
+							'beforeend',
+							'<br />inside iframe: ' + count++
+						)
+					}, 1000)
+				<\/script>
+			`).trim(),
 
-			script: `window.buttonClick = () => {
-    document.body.insertAdjacentHTML(
-        'afterbegin',
-        '<p>Polluted!</p>'
-    )
-}
+			script: stripIndent(`
+				window.buttonClick = () => {
+					document.body.insertAdjacentHTML(
+						'afterbegin',
+						'<p>Polluted!</p>'
+					)
+				}
 
-// click the button to see that this example pollutes the environment...
-document.body.insertAdjacentHTML(
-    'afterbegin',
-    '<button onclick="buttonClick()">Click to pollute...</button>'
-)
-`,
+				// click the button to see that this example pollutes the environment...
+				document.body.insertAdjacentHTML(
+					'afterbegin',
+					'<button onclick="buttonClick()">Click to pollute...</button>'
+				)
+			`).trim(),
 
-			scriptIframe: `document.head.innerHTML = \`
-    <style>
-        body {
-            background: skyblue;
-            width: 100%;
-            height: 100%;
-        }
-    </style>
-\`
+			scriptIframe: stripIndent(`
+				document.head.innerHTML = \`
+					<style>
+						body {
+							background: skyblue;
+							width: 100%;
+							height: 100%;
+						}
+					</style>
+				\`
 
-let count = 0
+				let count = 0
 
-setInterval(() => {
-    document.body.insertAdjacentHTML(
-        'beforeend',
-        '<br />inside iframe: ' + count++
-    )
-}, 1000)
-`,
+				setInterval(() => {
+					document.body.insertAdjacentHTML(
+						'beforeend',
+						'<br />inside iframe: ' + count++
+					)
+				}, 1000)
+			`).trim(),
 
-			code: `<template>
-    <h1>Count: {{count}}</h1>
-</template>
+			code: stripIndent(`
+				<template>
+					<h1>Count: {{count}}</h1>
+				</template>
 
-<style>
-    h1 {
-        background: deeppink;
-    }
-</style>
+				<style>
+					h1 {
+						background: deeppink;
+					}
+				</style>
 
-<script>
-    export default {
-        data: () => ({
-            count: 0,
-        }),
-        mounted() {
-            setInterval(() => {
-                this.count++
-            }, 200);
-        },
-    }
-<\/script>`,
+				<script>
+					export default {
+						data: () => ({
+							count: 0,
+						}),
+						mounted() {
+							setInterval(() => {
+								this.count++
+							}, 200);
+						},
+					}
+				<\/script>
+			`).trim(),
 
-			code2: `<template>
-    <h1>Count: {{count}}</h1>
-</template>
+			code2: stripIndent(`
+				<template>
+					<h1>Count: {{count}}</h1>
+				</template>
 
-<style>
-    /* style the body inside the iframe */
-    body {
-        margin: 0;
-        padding: 0;
-        width: 100%;
-        height: 100%;
-    }
-    h1 {
-        background: deeppink;
-    }
-</style>
+				<style>
+					/* style the body inside the iframe */
+					body {
+						margin: 0;
+						padding: 0;
+						width: 100%;
+						height: 100%;
+					}
+					h1 {
+						background: deeppink;
+					}
+				</style>
 
-<script>
-    export default {
-        data: () => ({
-            count: 0,
-        }),
-        mounted() {
-            setInterval(() => {
-                this.count++
-            }, 200);
-        },
-    }
-<\/script>`,
+				<script>
+					export default {
+						data: () => ({
+							count: 0,
+						}),
+						mounted() {
+							setInterval(() => {
+								this.count++
+							}, 200);
+						},
+					}
+				<\/script>
+			`).trim(),
 		}),
 	}
 </script>
